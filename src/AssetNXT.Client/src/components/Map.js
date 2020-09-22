0d0e27a31653598ceba9ab4294c709556af7fb2c
Rick <442124@student.fontys.nl>
2020-09-22 18:14:30 +0200

    Added Leaflet map component with default location pointing to Eindhoven, Mediamarkt.

---
+++ src/AssetNXT.Client/src/components/Map.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+export class MapNXT extends Component {
+	render() {
+
+		const state = { lat: 51.4417378, lng: 5.4750301, zoom: 13 }
+		const position = [state.lat, state.lng]
+
+		return (
+			<div className="map-container">
+				<Map center={position} zoom={state.zoom}
+					style={{ height: '800px' }}>
+					<TileLayer
+						tileSize={ 512 }
+						zoomOffset={ -1 }
+						attribution='&amp;copy <a href="https://www.maptiler.com/copyright/">Maptiler</a> contributors'
+						url="https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=HfiQgsMsSnorjEs2Sxek"
+					/>
+					<Marker position={position}>
+						<Popup>
+							It's a secret to everybody
+						</Popup>
+					</Marker>
+				</Map>
+			</div>
+        );
+	}
+}
