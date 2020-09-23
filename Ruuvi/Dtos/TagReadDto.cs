58f459f2563e43727f1c20b24d2a45abd37c8589
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-09-23 18:11:52 +0200

    Create Dtos

---
+++ Ruuvi/Dtos/TagReadDto.cs
@@ -0,0 +1,10 @@
+namespace Ruuvi.Dtos
+{
+    public class TagReadDto
+    {
+        public int Id {get; set;}
+        public string HowTo { get;set;}
+        public string Line { get; set;}
+    
+    }
+}
--- Ruuvi/Startup.cs
+++ Ruuvi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,8 +31,10 @@ namespace Ruuvi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // DB dependencies
             services.AddDbContext<RuuviDbContext>(options => options.UseMySql(Configuration.GetConnectionString("Default")));
 
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
                       
             services.AddControllers();
             
