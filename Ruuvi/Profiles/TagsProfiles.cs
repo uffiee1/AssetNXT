4bdc290660e105abace4c02edf61ddb70ad933fe
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-09-23 18:17:16 +0200

    Create Profile where I map the Model with the Dto

---
+++ Ruuvi/Profiles/TagsProfiles.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using Ruuvi.Dtos;
+using Ruuvi.Models;
+
+namespace Ruuvi.Profiles
+{
+    public class TagsProfile : Profile
+    {
+        public TagsProfile()
+        {
+            CreateMap<Tag, TagReadDto>();
+        }
+    }
+}
