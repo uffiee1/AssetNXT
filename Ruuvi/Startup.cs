d5b17d1b692752b418dee59eacd9c3ab0912e19b
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-09-25 11:57:08 +0200

    Set-up a Docker

--- Ruuvi/Startup.cs
+++ Ruuvi/Startup.cs
@@ -48,7 +48,6 @@ namespace Ruuvi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        // Middlewares
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             
