be323f8c00e4714a11ec96529795d6e6927ebe81
Ufuk Angay <u.angay@gmail.com>
2020-09-28 16:55:26 +0200

    Started with Specific Geographic Area-WIP
    @Zandzakje

--- src/AssetNXT.Client/src/components/Map.js
+++ src/AssetNXT.Client/src/components/Map.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, Circle , Polygon } from 'react-leaflet';
 
 export class MapNXT extends Component {
 	render() {
 
-		const state = { lat: 51.4417378, lng: 5.4750301, zoom: 13 }
-		const position = [state.lat, state.lng]
+        const state = { lat: 51.4417378, lng: 5.4750301, zoom: 13 }
+        const position = [state.lat, state.lng]
+        const circle = [51.461559, 5.477471]
+        const polygon = [[51.441250, 5.474571], [51.442160, 5.474900], [51.441399, 5.475700]]
 
 		return (
 			<div className="map-container">
@@ -22,6 +24,17 @@ export class MapNXT extends Component {
 							It's a secret to everybody
 						</Popup>
 					</Marker>
+
+                    /*Red Circle for the Map*/
+                    <Circle center={circle} 
+                    color='red' 
+                    fillColor='#f03' 
+                    fillOpacity={0.5} 
+                    radius={500} /> 
+                        
+                    /*Blue Triangle for the Location*/
+                    <Polygon positions={polygon}/>
+
 				</Map>
 			</div>
         );
