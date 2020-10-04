cb156a658e0a3138c3471a3e7f0d58452deae8a7
Rick <442124@student.fontys.nl>
2020-10-04 20:57:39 +0200

    Remove unnecessary template components.

--- src/AssetNXT.Client/src/components/Layout.js
+++ src/AssetNXT.Client/src/components/Layout.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
 import { MapNXT } from './Map';
 
 export class Layout extends Component {
