8b9b00a98da3726eb96465daa5766ace2bc754c0
Rick <442124@student.fontys.nl>
2020-10-05 15:18:48 +0200

    Reworked out-of-bounds models, logic and unit tests for improved readability.

---
+++ src/AssetNXT/Logic/IIntersector.cs
@@ -0,0 +1,9 @@
+﻿using AssetNXT.Models;
+
+namespace AssetNXT.Logic
+{
+    public interface IIntersector
+    {
+        bool IntersectsWith(Point point);
+    }
+}
---
+++ src/AssetNXT/Models/Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using AssetNXT;
+using AssetNXT.Logic;
+
+namespace AssetNXT.Models
+{
+    public class Triangle : IIntersector
+    {
+        public Point A { get; set; }
+
+        public Point B { get; set; }
+
+        public Point C { get; set; }
+
+        public bool IntersectsWith(Point point)
+        {
+            var a = Sign(point, A, B);
+            var b = Sign(point, B, C);
+            var c = Sign(point, C, A);
+
+            var hasNeg = (a < 0) || (b < 0) || (c < 0);
+            var hasPos = (a > 0) || (b > 0) || (c > 0);
+
+            return !(hasNeg && hasPos);
+        }
+
+        public static double Sign(Point a, Point b, Point c)
+        {
+            return ((a.X - c.X) * (b.Y - c.Y)) - ((b.X - c.X) * (a.Y - c.Y));
+        }
+    }
+}
