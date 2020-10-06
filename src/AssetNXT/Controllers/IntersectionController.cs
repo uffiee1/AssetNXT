c19f64df3480630712e5aedef08c8adfcf3f3937
Spaninks,Luka L <l.spaninks@student.fontys.nl>
2020-10-06 10:56:56 +0200

    Services and controllers folder added

---
+++ src/AssetNXT/Controllers/IntersectionController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetNXT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntersectionController : Controller
+    {
+        // GET: api/Intersection
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new List<bool>() { true, false, false, true, false });
+        }
+    }
+}
