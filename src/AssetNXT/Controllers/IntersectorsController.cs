34fc76e5e5d1d3822da9c99e6a140a002bb0547b
Tony <tony.zhou125@gmail.com>
2020-10-06 15:35:49 +0200

    Update IntersectorsController.cs
    Added API endpoints for:
    -Circles
    -Triangles

--- src/AssetNXT/Controllers/IntersectorsController.cs
+++ src/AssetNXT/Controllers/IntersectorsController.cs
@@ -10,12 +10,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AssetNXT.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class IntersectorsController : Controller
     {
+        [Route("api/[controller]/Triangles")]
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Triangles()
+        {
+            var point = new Point { X = 5, Y = 8 };
+            var intersectors = new List<Triangle>
+            {
+                CreateSampleTriangle01(),
+                CreateSampleTriangle02()
+            };
+
+            return Json(new
+            {
+                Point = point,
+                Triangles = intersectors.Select(triangle => new
+                {
+                    Triangle = triangle,
+                    IntersectsWith = triangle.IntersectsWith(point)
+                })
+            });
+        }
+
+        [Route("api/[controller]/Circles")]
+        [HttpGet]
+        public IActionResult Circles()
         {
             var point = new Point { X = 5, Y = 8 };
             var intersectors = new List<Circle>
@@ -26,9 +48,11 @@ namespace AssetNXT.Controllers
 
             return Json(new
             {
-                Point = point, Circles = intersectors.Select(circle => new
+                Point = point,
+                Circles = intersectors.Select(circle => new
                 {
-                    Circle = circle, OutOfBounds = !circle.IntersectsWith(point)
+                    Circle = circle,
+                    IntersectsWith = circle.IntersectsWith(point)
                 })
             });
         }
@@ -41,6 +65,26 @@ namespace AssetNXT.Controllers
         private Circle CreateSampleCircle02()
         {
             return new Circle { Position = (25, 10), Radius = 35.0 };
+        }
+
+        private Triangle CreateSampleTriangle01()
+        {
+            return new Triangle
+            {
+                A = new Point { X = 0, Y = 0 },
+                B = new Point { X = 3, Y = 2 },
+                C = new Point { X = 6, Y = 0 },
+            };
+        }
+
+        private Triangle CreateSampleTriangle02()
+        {
+            return new Triangle
+            {
+                A = new Point { X = 6, Y = 0 },
+                B = new Point { X = 3, Y = 2 },
+                C = new Point { X = 0, Y = 0 },
+            };
         }
     }
 }
