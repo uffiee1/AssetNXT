b0ec524177750c45a2a62ad93dc7610d57e3775c
Rick <442124@student.fontys.nl>
2020-10-06 14:45:17 +0200

    Sample JSON response for retrieving intersectors.

---
+++ src/AssetNXT/Controllers/IntersectorsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AssetNXT.Logic;
+using AssetNXT.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetNXT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntersectorsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var point = new Point { X = 5, Y = 8 };
+            var intersectors = new List<Circle>
+            {
+                CreateSampleCircle01(),
+                CreateSampleCircle02()
+            };
+
+            return Json(new
+            {
+                Point = point, Circles = intersectors.Select(circle => new
+                {
+                    Circle = circle, OutOfBounds = !circle.IntersectsWith(point)
+                })
+            });
+        }
+
+        private Circle CreateSampleCircle01()
+        {
+            return new Circle { Position = (0, 0), Radius = 25.0 };
+        }
+
+        private Circle CreateSampleCircle02()
+        {
+            return new Circle { Position = (25, 10), Radius = 35.0 };
+        }
+    }
+}
