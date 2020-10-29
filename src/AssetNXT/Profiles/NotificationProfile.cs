adba7edfa623c6c4fd5bd8ad1d8b03a916de4f58
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-10-29 19:54:39 +0100

    Create NotificationProfile.cs

---
+++ src/AssetNXT/Profiles/NotificationProfile.cs
@@ -0,0 +1,17 @@
+﻿using AssetNXT.Dtos;
+using AssetNXT.Models.Data;
+
+using AutoMapper;
+
+namespace AssetNXT.Profiles
+{
+    public class NotificationProfile : Profile
+    {
+        public NotificationProfile()
+        {
+            CreateMap<Notification, NotificationReadDto>();
+            CreateMap<NotificationCreateDto, Notification>();
+            CreateMap<Notification, NotificationCreateDto>();
+        }
+    }
+}
