eb534520a45531b737e7f95cfb0571133e0dc9d1
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-10-29 20:43:17 +0100

    Create ConstrainProfile.cs

---
+++ src/AssetNXT/Profiles/ConstrainProfile.cs
@@ -0,0 +1,17 @@
+﻿using AssetNXT.Dtos;
+using AssetNXT.Models.Data;
+
+using AutoMapper;
+
+namespace AssetNXT.Profiles
+{
+    public class ConstrainProfile : Profile
+    {
+        public ConstrainProfile()
+        {
+            CreateMap<Constrain, ConstrainReadDto>();
+            CreateMap<ConstrainCreateDto, Constrain>();
+            CreateMap<Constrain, ConstrainCreateDto>();
+        }
+    }
+}
