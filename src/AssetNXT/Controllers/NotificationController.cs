fe94c323fb2d754c0dd1851d0f8814bcf3430d69
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-10-29 19:58:48 +0100

    Create NotificationController.cs

---
+++ src/AssetNXT/Controllers/NotificationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AssetNXT.Dtos;
+using AssetNXT.Models.Data;
+using AssetNXT.Repository;
+
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetNXT.Controllers
+{
+    [Route("api/notifications")]
+    public class NotificationController
+    {
+        private readonly IMongoDataRepository<RuuviStation> _repository;
+        private readonly IMapper _mapper;
+
+        public NotificationController(IMongoDataRepository<RuuviStation> repository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+    }
+}
