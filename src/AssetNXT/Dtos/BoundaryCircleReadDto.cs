14365a4146c159cf49cd551c3403bd42d6112b24
Zandzakje <DamianTomasila@hotmail.com>
2020-11-02 14:46:45 +0100

    Added boundary configurations

---
+++ src/AssetNXT/Dtos/BoundaryCircleReadDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetNXT.Dtos
+{
+    public class BoundaryCircleReadDto
+    {
+        public string Id { get; set; }
+
+        public double Radius { get; set; }
+
+        public string Colour { get; set; }
+
+        public double Accuracy { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ src/AssetNXT/Models/Data/Boundary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AssetNXT.Models.Data
+{
+    public class Boundary : Document
+    {
+        [BsonElement]
+        public string Colour { get; set; }
+
+        [BsonElement]
+        public Location Location { get; set; }
+    }
+}
