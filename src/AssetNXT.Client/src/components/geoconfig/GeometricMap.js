95b8579a3e34748d2d9e860a738d0983fba590dd
Rick <442124@student.fontys.nl>
2020-11-24 22:39:52 +0100

    Update stateHasChanged

--- src/AssetNXT.Client/src/components/geoconfig/GeometricMap.js
+++ src/AssetNXT.Client/src/components/geoconfig/GeometricMap.js
@@ -7,35 +7,58 @@ import './GeometricMap.css';
 export default class GeometricMap extends Component {
 
   state = {
-    markers: this.props.positions,
-    selectedIndex: this.props.selectedIndex
-  }
-
-  selectMarker(selectedIndex) {
-    this.setState({selectedIndex});
-    if (this.props.stateHasChanged) {
-      this.props.stateHasChanged(this.state);
-    }
+    selected: false,
+    selectedIndex: Number.NaN,
+    markers: this.props.positions
   }
 
   addMarker(position) {
+    console.log('click');
+    const index = this.state.markers.length;
     const {markers} = this.state;
     markers.push(position);
 
-    this.setState({markers, selectedIndex: markers.length - 1});
-    if (this.props.stateHasChanged) {
-      this.props.stateHasChanged(this.state);
-    }
+    this.setState({markers, selectedIndex: index});
+    this.invokeStateHasChanged();
+  }
+ 
+  removeMarker(index) {
+    const {markers} = this.state;
+    markers.splice(index, 1);
+
+    this.setState({markers, selectedIndex: Number.NaN});
+    this.invokeStateHasChanged();
   }
 
-  updateMarker(position, index) {
+  updateMarker(index, position) {
     const {markers} = this.state;
     markers[index] = position;
 
     this.setState({markers});
+    this.invokeStateHasChanged();
+  }
+
+  invokeStateHasChanged() {
     if (this.props.stateHasChanged) {
       this.props.stateHasChanged(this.state);
     }
+  }
+
+  render() {
+    return (
+      <Map zoom={this.props.zoom}
+        center={this.props.center}
+        onclick={(e) => { this.addMarker(e.latlng) }}>
+
+        <TileLayer tileSize={512} zoomOffset={-1}
+          url='https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=HfiQgsMsSnorjEs2Sxek'
+          attribution='&amp;copy <a href="https://www.maptiler.com/copyright/">Maptiler</a> contributors' />
+
+          {this.state.markers.map((position, idx) => this.renderMarker(position, idx))}
+          <Polyline positions={this.state.markers.map(position => [position.lat, position.lng])}/>
+
+      </Map>
+    );
   }
 
   renderMarker(position, idx) {
@@ -46,42 +69,30 @@ export default class GeometricMap extends Component {
         key={`marker-${idx}`}
         ondrag={this.dragHandler}
         ondragend={this.dragEndHandler}
-        ondragstart={this.dragStartHandler}
-        onclick={this.clickHandler}>
+        ondragstart={this.dragStartHandler}>
       </Marker>
     );
   }
 
-  render() {
-    return (
-
-      <Map zoom={this.props.zoom}
-        center={this.props.center}
-        onclick={(e) => this.addMarker(e.latlng)}>
-
-        <TileLayer tileSize={512} zoomOffset={-1}
-          url='https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=HfiQgsMsSnorjEs2Sxek'
-          attribution='&amp;copy <a href="https://www.maptiler.com/copyright/">Maptiler</a> contributors' />
-
-
-        {this.state.markers.map((position, idx) => this.renderMarker(position, idx))}
-        <Polyline positions={this.state.markers.map(position => [position.lat, position.lng])}/>
-
-      </Map>
-    );
+  dragHandler = (__e) => {
+    console.log('drag');
+    const targetOptions = __e.target.options;
+    this.updateMarker(targetOptions.index, __e.latlng);
   }
 
-  clickHandler = (e) => {
-    this.selectMarker(e.target.options.index);
+  dragEndHandler = (__e) => {
+    console.log('drag-end');
+    const targetOptions = __e.target.options;
+    this.setState({selected: false, selectedIndex: targetOptions.index});
+    this.invokeStateHasChanged();
   }
 
-  dragHandler = (e) => {
-    this.updateMarker(e.latlng, e.target.options.index);
-  }
+  dragStartHandler = (__e) => {
+    console.log('drag-start');
+    const targetOptions = __e.target.options;
+    this.setState({selected: true, selectedIndex: targetOptions.index});
+    this.invokeStateHasChanged();
 
-  dragEndHandler = (e) => {
-  }
-
-  dragStartHandler = (e) => {
+    console.log(this.state.selected);
   }
 }
