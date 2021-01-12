4754f295a1c8571ed54c1a2fa7e12760b3ee0efa
Tony <tony.zhou125@gmail.com>
2021-01-12 11:04:06 +0100

    Added remaining API endpoints

--- src/AssetNXT.Client/src/api/agent.js
+++ src/AssetNXT.Client/src/api/agent.js
@@ -2,7 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
 // Url needs to be changed when hosted in the cloud.
-axios_1.default.defaults.baseURL = "https://localhost:5001/api";
+var baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+axios_1.default.defaults.baseURL = baseUrl + "/api";
 var responseBody = function (response) { return response.data; };
 var requests = {
     get: function (url) { return axios_1.default.get(url).then(responseBody); },
@@ -16,11 +17,30 @@ var requests = {
         return axios_1.default.delete(url).then(responseBody);
     },
 };
-var Geometric = {
-    routes: function () { return requests.get("/routes"); }
+var Stations = {
+    getStationsByDeviceId: function (id) { return requests.get("/stations/all/" + id); },
+    getStations: function () { return requests.get("/stations"); },
+};
+var Notification = {};
+var Routes = {
+    getRoutes: function () { return requests.get("/routes"); },
+    applyRoute: function (id, route) { return requests.put("/routes/" + id, route); },
+    createRoute: function (route) { return requests.put("/routes", route); },
+    deleteRoute: function (id) { return requests.delete("/routes/" + id); },
+    editRoute: function (id, route) { return requests.put("/constraints/" + id, route); },
+};
+var Telemetric = {
+    getConstraints: function () { return requests.get("/constraints"); },
+    applyConstraints: function (sla) { return requests.put("/constraints", sla); },
+    createConstraints: function (sla) { return requests.post("/constraints", sla); },
+    deleteConstraints: function (id) { return requests.delete("/constraints/" + id); },
+    editConstraints: function (id, sla) { return requests.put("/constraints/" + id, sla); }
 };
 // eslint-disable-next-line import/no-anonymous-default-export
 exports.default = {
-    Geometric: Geometric
+    Notification: Notification,
+    Stations: Stations,
+    Routes: Routes,
+    Telemetric: Telemetric
 };
 //# sourceMappingURL=agent.js.map
