7f0df6075b9afdf59aea706ecc3e9a06db0e27a2
Tony <tony.zhou125@gmail.com>
2021-01-14 01:00:26 +0100

    Removed unused import

--- src/AssetNXT.Client/src/pages/NotificationPage.js
+++ src/AssetNXT.Client/src/pages/NotificationPage.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import NotificationTable from "../components/notification/NotificationTable";
 import Layout from "../components/Layout";
 
